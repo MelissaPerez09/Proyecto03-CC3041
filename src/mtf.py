9743ada284d79745d3c71e5264533ed1a921c4c8
Melissa Pérez <per21385@uvg.edu.gt>
2024-05-14 01:02:32 -0600

    finished MTF

--- src/mtf.py
+++ src/mtf.py
@@ -9,30 +9,49 @@ Melissa Pérez Alarcón, 21385
 initial_list = [0, 1, 2, 3, 4]
 
 # --- 1 ---
-request_sequence = [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
+#request_sequence = [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
 
 # --- 2 ---
-# request_sequence = [4, 3, 2, 1, 0, 1, 2, 3, 4, 3, 2, 1, 0, 1, 2, 3, 4]
+#request_sequence = [4, 3, 2, 1, 0, 1, 2, 3, 4, 3, 2, 1, 0, 1, 2, 3, 4]
 
+# -- 3 ---
+#request_sequence = [0] * 20
+
+# -- 4 ---
+#request_sequence = [4, 3, 2, 1, 0, 4, 3, 2, 1, 0, 4, 3, 2, 1, 0, 4, 3, 2, 1, 0, 4, 3, 2, 1, 0]
+
+# -- 5 ---
+request_sequence = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+
+
+# Función que simula el algoritmo Move-to-Front
 def move_to_front(sequence, requests):
+    
+    # Se crea una copia de la lista de configuración inicial
     config_list = sequence.copy()
     total_cost = 0
-    costs = []
-    configurations = []
+    costs = []                                      # Lista que almacena los costos de cada solicitud
+    configurations = []                             # Lista que almacena las configuraciones de la lista
     
+    # Se recorre la secuencia de solicitudes
     for request in requests:
-        index = config_list.index(request)
-        cost = index + 1
-        total_cost += cost
-        config_list.pop(index)
-        config_list.insert(0, request)
-        costs.append(cost)
-        configurations.append(config_list.copy())
+        index = config_list.index(request)          # Se obtiene el índice de la solicitud en la lista
+        
+        cost = index + 1                            # Se calcula el costo de la solicitud
+        total_cost += cost                          # Se suma el costo al costo total
+        
+        config_list.pop(index)                      # Se elimina la solicitud de la lista
+        config_list.insert(0, request)              # Se inserta la solicitud al inicio de la lista
+        
+        costs.append(cost)                          # Se agrega el costo a la lista de costos
+        configurations.append(config_list.copy())   # Se agrega la configuración de la lista a la lista de configuraciones
     
     return total_cost, configurations, costs
 
+# Se ejecuta el algoritmo y se obtienen los resultados
 total_cost, configurations, costs = move_to_front(initial_list, request_sequence)
 
+# Impresión de resultados
 print(f"Lista de configuración inicial: {initial_list}\n")
 
 for element, cost, config in zip(request_sequence, costs, configurations):
